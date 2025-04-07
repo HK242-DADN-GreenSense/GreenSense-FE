79982552d75c930fc49129a94f9e36b433b4db63
Duong Hai Lam <lam.duong210425@hcmut.edu.vn>
2025-04-07 17:00:37 +0700

    feat: add Temperature Card

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./styles/App.css";
 import SoilMoistureCard from "./components/SoilMoistureCard";
+import TemperatureCard from "./components/TemperatureCard";
 
 function App() {
     return (
         <>
             <SoilMoistureCard />
-            <SoilMoistureCard />
+            <TemperatureCard />
             <SoilMoistureCard />
         </>
     );

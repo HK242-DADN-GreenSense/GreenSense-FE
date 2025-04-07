83eb46966d0ca331429a9bc668c3a6be241d3a44
Duong Hai Lam <lam.duong210425@hcmut.edu.vn>
2025-04-07 17:06:51 +0700

    feat: add Light Card

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import viteLogo from "/vite.svg";
 import "./styles/App.css";
 import SoilMoistureCard from "./components/SoilMoistureCard";
 import TemperatureCard from "./components/TemperatureCard";
+import LightCard from "./components/LightCard";
 
 function App() {
     return (
         <>
             <SoilMoistureCard />
             <TemperatureCard />
-            <SoilMoistureCard />
+            <LightCard />
         </>
     );
 }

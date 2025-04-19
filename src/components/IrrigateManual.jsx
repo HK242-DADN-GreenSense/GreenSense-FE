b4eaf6c0f1f9e34722969e5939fc446e8d77d859
Duong Hai Lam <lam.duong210425@hcmut.edu.vn>
2025-04-19 18:57:59 +0700

    feat: add some manual components

---
+++ src/components/IrrigateManual.jsx
@@ -0,0 +1,29 @@
+import TextField from "@mui/material/TextField";
+
+const IrrigationManualAction = () => {
+    return (
+        <>
+            <div className="h-[100%] w-[100%] col-span-1 row-span-1 grid grid-cols-1 grid-rows-1 items-center text-center">
+                <div>
+                    <TextField
+                        label="Duration (s)"
+                        variant="outlined"
+                        type="number"
+                    />
+                </div>
+            </div>
+            <div className="h-[100%] w-[100%] col-span-1 row-span-1 grid grid-cols-1 grid-rows-1 items-center text-center">
+                <div>
+                    <buttom
+                        onClick={() => console.log("click")}
+                        className="bg-[#008000] px-16 py-2 rounded-4xl text-white min-w-[100%] hover:cursor-pointer font-mono"
+                    >
+                        Irrigate
+                    </buttom>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default IrrigationManualAction;

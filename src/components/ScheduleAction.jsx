faf69b487d153862f2d5ee34e3a61bd97f09e9b2
Duong Hai Lam <lam.duong210425@hcmut.edu.vn>
2025-04-20 20:41:17 +0700

    feat: complete temperature schedule mode

--- src/components/ScheduleAction.jsx
+++ src/components/ScheduleAction.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import dayjs from "dayjs";
 
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -9,20 +9,37 @@ import Select from "@mui/material/Select";
 
 import garbage from "../assets/garbage-icon.svg";
 
-const ScheduleAction = () => {
-    const [action, setAction] = useState(1);
+const ScheduleAction = ({ setting, setSetting, index }) => {
+    const handleTimeChange = (e) => {
+        setting.schedule[index].time = e.$d;
+        setSetting({ ...setting });
+    };
+
+    const handleActionChange = (e) => {
+        setting.schedule[index].action = e.target.value;
+        setSetting({ ...setting });
+    };
+
+    const handleDelete = (e) => {
+        setting.schedule = setting.schedule.filter((_, i) => i !== index);
+        setSetting({ ...setting });
+    };
     return (
         <div className="flex justify-center items-center">
             <div className="w-[30%] p-2">
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <TimePicker label="Time" />
+                    <TimePicker
+                        value={dayjs(setting.schedule[index].time)}
+                        onChange={(e) => handleTimeChange(e)}
+                        label="Time"
+                    />
                 </LocalizationProvider>
             </div>
             <div className="w-[20%]">
                 <FormControl sx={{ width: 100 }}>
                     <Select
-                        value={action}
-                        onChange={(e) => setAction(e.target.value)}
+                        value={setting.schedule[index].action}
+                        onChange={(e) => handleActionChange(e)}
                     >
                         <MenuItem value={1}>Open</MenuItem>
                         <MenuItem value={0}>Close</MenuItem>
@@ -31,6 +48,7 @@ const ScheduleAction = () => {
             </div>
             <div className="max-h-[10%] max-w-[10%] inline-block m-4">
                 <img
+                    onClick={(e) => handleDelete(e)}
                     src={garbage}
                     className="h-[50%] w-[50%] inline-block hover:cursor-pointer"
                 ></img>
---
+++ src/components/TemperatureSchedule.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+
+import Typography from "@mui/material/Typography";
+import Slider from "@mui/material/Slider";
+
+import Schedule from "./Schedule";
+import ScheduleAction from "./ScheduleAction";
+import useDebounce from "../hooks/useDebounce";
+
+const TemperatureSchedule = ({ valueRange }) => {
+    const [min, max] = valueRange;
+    const [setting, setSetting] = useState(
+        JSON.parse(localStorage.getItem("temperatureScheduleSetting")) || {
+            expectedRange: [0, 10],
+            currentAction: "init",
+            action: "warning",
+            schedule: [{ time: new Date(), action: 1 }],
+        }
+    );
+
+    const debouncedSetting = useDebounce(setting, 500);
+
+    const marks = setting.expectedRange.map((value) => {
+        return { value: value, label: value };
+    });
+
+    useEffect(() => {
+        localStorage.setItem(
+            "temperatureScheduleSetting",
+            JSON.stringify(debouncedSetting)
+        );
+        toast.success("Temperature setting updated");
+    }, [debouncedSetting]);
+    return (
+        <>
+            <div className="h-[100%] w-[100%] col-span-1 row-span-1 grid grid-cols-1 grid-rows-1 items-center text-center">
+                <div>
+                    <Typography sx={{ fontFamily: "mono" }} gutterBottom>
+                        Expected range (°C)
+                    </Typography>
+                    <Slider
+                        sx={{
+                            width: 300,
+                            margin: "8 auto",
+                        }}
+                        min={min}
+                        max={max}
+                        value={setting.expectedRange}
+                        onChange={(e, newRange) => {
+                            setSetting((prev) => ({
+                                ...prev,
+                                expectedRange: newRange,
+                            }));
+                        }}
+                        marks={marks}
+                        aria-label="Always visible"
+                    />
+                </div>
+            </div>
+            <Schedule setting={setting} setSetting={setSetting}>
+                {setting.schedule.map((_, index) => (
+                    <ScheduleAction
+                        key={index}
+                        setting={setting}
+                        setSetting={setSetting}
+                        index={index}
+                    />
+                ))}
+            </Schedule>
+            <ToastContainer />
+        </>
+    );
+};
+
+export default TemperatureSchedule;

778327f7ebb329e671482068bf3a9ee4578f0bcd
Duong Hai Lam <lam.duong210425@hcmut.edu.vn>
2025-04-20 13:10:47 +0700

    feat: add control action in manual mode for temperature and lighting control

---
+++ src/components/TempAndLightManual.jsx
@@ -0,0 +1,13 @@
+import ManualSwitch from "./ManualSwitch";
+
+const TempAndLigntManual = () => {
+    return (
+        <div className="h-[100%] w-[100%] col-span-1 row-span-2 grid grid-cols-1 grid-rows-1 items-center text-center">
+            <div className="flex justify-center align-items-center text-center items-center">
+                <ManualSwitch />
+            </div>
+        </div>
+    );
+};
+
+export default TempAndLigntManual;
